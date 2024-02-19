0328615d54b5c4be7c3c068ddc21a6208afa6ee4
Chedvihas <chedvi@Chedvihass-MacBook-Pro.local>
2024-02-18 22:05:20 -0700

    Auto-commit changes

--- git.sh
+++ git.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# Set up git user details (replace with your details)
-# git config --global user.email "you@example.com"
-# git config --global user.name "Your Name"
-
 # Add all changes and commit
 git add .
 git commit -m "Auto-commit changes"
