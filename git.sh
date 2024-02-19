001918b72550246dbda5c7b3c8970756d6dc1218
Chedvihas <chedvi@Chedvihass-MacBook-Pro.local>
2024-02-19 00:13:01 -0700

    Cron checks each minute and commits these new changes

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 cd /Users/chedvi/Desktop/CI-CDTEST/
 git remote set-url origin https://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@github.com/Chedvihas/CI-CD-test.git
 git -C /Users/chedvi/Desktop/CI-CDTEST/ add .
-git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron committed these new changes"
+git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron checks each minute and commits these new changes"
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push --set-upstream origin main
 # Push changes to remote repository
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push # Replace 'main' with your branch name if necessary
