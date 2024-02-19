9763f19a491fc89989712653f2b4232a4f18dbee
Chedvihas <chedvi@Chedvihass-MacBook-Pro.local>
2024-02-18 23:57:00 -0700

    Cron committed these new changes

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 # Add all changes and commit
 cd /Users/chedvi/Desktop/CI-CDTEST/
-git add .
-git commit -m "Cron committed these changes"
-git push --set-upstream origin main
+git -C /Users/chedvi/Desktop/CI-CDTEST/ add .
+git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron committed these new changes"
+git -C /Users/chedvi/Desktop/CI-CDTEST/ push --set-upstream origin main
 # Push changes to remote repository
-git push # Replace 'main' with your branch name if necessary
+git -C /Users/chedvi/Desktop/CI-CDTEST/ push # Replace 'main' with your branch name if necessary
