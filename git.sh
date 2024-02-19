cd1023a737db03fad176f2c1c91418ea142e6b4b
Chedvihas <chedvi@Chedvihass-MacBook-Pro.local>
2024-02-19 00:11:00 -0700

    Cron committed these new changes

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 # Add all changes and commit
 cd /Users/chedvi/Desktop/CI-CDTEST/
+git remote set-url origin https://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@github.com/Chedvihas/CI-CD-test.git
 git -C /Users/chedvi/Desktop/CI-CDTEST/ add .
 git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron committed these new changes"
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push --set-upstream origin main
