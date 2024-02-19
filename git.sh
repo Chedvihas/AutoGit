46db0ceb21fbbd78fa851cca75399507cefffb64
Chedvihas <chedvi4999@gmail.com>
2024-02-19 00:57:00 -0700

    Leetcode problem commit at 19th Feb 12:57 AM

--- git.sh
+++ git.sh
@@ -1,7 +1,8 @@
 # Add all changes and commit
 cd /Users/chedvi/Desktop/CI-CDTEST/
 git -C /Users/chedvi/Desktop/CI-CDTEST/ add .
-git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron commit at 12:54AM"
+current_datetime=$(date +"%dth %b %I:%M %p")
+git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Leetcode problem commit at $current_datetime"
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push --set-upstream origin main
 # Push changes to remote repository
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push # Replace 'main' with your branch name if necessary
