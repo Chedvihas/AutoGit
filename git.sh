25d6689c1f5fcae7f31f78416ed8751bae5e1621
Chedvihas <chedvi4999@gmail.com>
2024-02-19 00:54:00 -0700

    Cron commit at 12:54AM

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 # Add all changes and commit
 cd /Users/chedvi/Desktop/CI-CDTEST/
 git -C /Users/chedvi/Desktop/CI-CDTEST/ add .
-git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron checks each minute and commits these new changes"
+git -C /Users/chedvi/Desktop/CI-CDTEST/ commit -m "Cron commit at 12:54AM"
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push --set-upstream origin main
 # Push changes to remote repository
 git -C /Users/chedvi/Desktop/CI-CDTEST/ push # Replace 'main' with your branch name if necessary
