b97c41b581ead811cc03a7ad487f50f788a71973
Chedvihas <chedvi@Chedvihass-MacBook-Pro.local>
2024-02-18 23:47:20 -0700

    Auto-commit changes

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@
 cd /Users/chedvi/Desktop/CI-CDTEST/
 git add .
 git commit -m "Auto-commit changes"
-
+git push --set-upstream origin main
 # Push changes to remote repository
 git push origin main  # Replace 'main' with your branch name if necessary
